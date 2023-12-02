93187af5c445bc8de19db5ce7c8204132d01966b
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-02 18:05:39 +0600

    protecting my enviroment from abuse

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+
 export default function Home() {
   return (
     <div className="">
-      <p className='text-3xl font-bold text-indigo-500'>Discord clone</p>
-      <Button >
-        click me
-      </Button>
-    
+      <p className='text-3xl font-bold text-indigo-500'>This is a protected route.</p>
+      
     </div>
       )
 }
