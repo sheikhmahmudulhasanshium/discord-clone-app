d58f92b8745d357bff518b3bae78d8c4af81f17b
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-05 04:37:08 +0600

    Form  and other components added

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,96 @@
+"use client"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { 
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "../ui/dialog"
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+    
+} from "@/components/ui/form"
+
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { useEffect, useState } from "react"
+
+const formSchema=z.object({
+    name: z.string().min(1,{
+        message: "Server name is required."
+    }),
+    imageUrl:z.string().min(1,
+        {
+            message: "Server image is required"
+        })
+})
+export const InitialModal=()=>{
+    const [isMounted,setIsMounted]=useState(false)
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+    const form=useForm({
+        resolver:zodResolver(formSchema),
+        defaultValues:{
+            name: "",
+            imageUrl:"",
+        }
+    })
+const isLoading=form.formState.isSubmitting
+const onSubmit = async (values:z.infer<typeof formSchema>)=>{
+    console.log(values)
+}
+if(!isMounted){return null}
+    return(
+        <Dialog open>
+            <DialogContent className="p-0 overflow-auto text-black bg-white">
+                <DialogHeader className="px-6 pt-8">
+                    <DialogTitle className="text-2xl text-center">Customize your server</DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500"> Make your server personality with name and an image. You can always change it later</DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="px-6 space-y-8">
+                            <div className="flex items-center justify-center text-center">
+                                TODO: Image Upload
+                            </div>
+                            <FormField 
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                               <FormItem>
+                                 <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+                                    Server name
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Input 
+                                    disabled={isLoading}
+                                    className="text-black border-0 bg-zinc-300/50 focus-visible:ring-0 focus-visible:ring-offset-0"
+                                    placeholder="Enter Server name"
+                                    {...field}
+                                    />
+                                 </FormControl>
+                                 <FormMessage></FormMessage>
+                               </FormItem> 
+                            )}
+                            />
+                        </div>
+                        <DialogFooter className="px-6 py-4 bg-gray-100">
+                            <Button variant={"primary"} disabled={isLoading}>Create</Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        shium: "hover:text-2xl"
+        shium: "hover:text-2xl",
+        primary: "bg-indigo-500 text-white hover:bg-indigo-500/90",
       },
       size: {
         default: "h-10 px-4 py-2",
