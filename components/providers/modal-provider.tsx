e33f010c9678a52f8972e012fcea43640b62ea30
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-07 04:02:18 +0600

    server settings added successfully

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal"
 import { InviteModal } from "../modals/invite-modal";
+import { EditServerModal } from "../modals/edit-server-modal";
 const ModalProvider = () => {
     const [isMounted,setIsMounted]=useState(false)
     useEffect(()=>{
@@ -14,6 +15,7 @@ const ModalProvider = () => {
         <>
         <CreateServerModal/>
         <InviteModal/>
+        <EditServerModal/>
         </>
      );
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -38,7 +38,8 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem 
+                    <DropdownMenuItem onClick={() => onOpen("editServer", { server })}
+
                     className="px-3 py-2 text-sm cursor-pointer dark:text-indigo-800">
                         
                         Server Settings
