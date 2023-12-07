ac2b8240a08471fd241d29bfc2dc61643f1fba14
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-08 00:04:15 +0600

    manage members added (buggy-sometimes)

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal"
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
+import { MembersModal } from "../modals/members-modal";
 const ModalProvider = () => {
     const [isMounted,setIsMounted]=useState(false)
     useEffect(()=>{
@@ -16,6 +17,7 @@ const ModalProvider = () => {
         <CreateServerModal/>
         <InviteModal/>
         <EditServerModal/>
+        <MembersModal/>
         </>
      );
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -16,75 +16,84 @@ export const ServerHeader = ({
     role,
 }:ServerHeaderProps) => {
     const {onOpen}=useModal()
-    const isAdmin=role===MemberRole.ADMIN
-    const isModerator=isAdmin||role===MemberRole.MODERATOR
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
     
-    return ( 
+    return (
         <DropdownMenu>
-            <DropdownMenuTrigger className="focus:outline-none" asChild>
-                <button className="flex items-center w-full h-12 px-3 font-semibold transition border-b-2 border-neutral-200 text-md dark:border-neutral-900 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50">
-                    {server.name}
-                    <ChevronDown className="w-5 h-5 ml-auto"/>
-                </button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-300 space-y-[2px]">
-                {isModerator && (
-                    <DropdownMenuItem onClick={()=>{onOpen("invite",{server})}}
-                    className="px-3 py-2 text-sm text-indigo-600 cursor-pointer dark:text-indigo-800">
-                        
-                        Invite People
-                        <UserPlus className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {isAdmin && (
-                    <DropdownMenuItem onClick={() => onOpen("editServer", { server })}
-
-                    className="px-3 py-2 text-sm cursor-pointer dark:text-indigo-800">
-                        
-                        Server Settings
-                        <Settings className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {isAdmin && (
-                    <DropdownMenuItem 
-                    className="px-3 py-2 text-sm cursor-pointer dark:text-indigo-800">
-                        
-                        Manage Members
-                        <Users className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {isModerator && (
-                    <DropdownMenuItem 
-                    className="px-3 py-2 text-sm cursor-pointer dark:text-indigo-800">
-                        
-                        Create Channel
-                        <PlusCircle className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {isModerator &&(
-                    <DropdownMenuSeparator/>
-                )}
-                {isAdmin&&(
-                    <DropdownMenuItem 
-                    className="px-3 py-2 text-sm cursor-pointer text-rose-500">
-                        
-                        Delete Server
-                        <Trash className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                    
-                )}
-                {!isAdmin&&(
-                    <DropdownMenuItem 
-                    className="px-3 py-2 text-sm cursor-pointer text-rose-500">
-                        
-                        Leave Server
-                        <LogOut className="w-4 h-4 ml-auto"/>
-                    </DropdownMenuItem>
-                    
-                )}
-            </DropdownMenuContent>
+          <DropdownMenuTrigger
+            className="focus:outline-none" 
+            asChild
+          >
+            <button
+              className="flex items-center w-full h-12 px-3 font-semibold transition border-b-2 text-md border-neutral-200 dark:border-neutral-800 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
+            >
+              {server.name}
+              <ChevronDown className="w-5 h-5 ml-auto" />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+          >
+            {isModerator && (
+              <DropdownMenuItem
+                onClick={() => onOpen("invite", { server })}
+                className="px-3 py-2 text-sm text-indigo-600 cursor-pointer dark:text-indigo-400"
+              >
+                Invite People
+                <UserPlus className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+            {isAdmin && (
+              <DropdownMenuItem
+                onClick={() => onOpen("editServer", { server })}
+                className="px-3 py-2 text-sm cursor-pointer"
+              >
+                Server Settings
+                <Settings className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+            {isAdmin && (
+              <DropdownMenuItem
+                onClick={() => onOpen("members", { server })}
+                className="px-3 py-2 text-sm cursor-pointer"
+              >
+                Manage Members
+                <Users className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+            {isModerator && (
+              <DropdownMenuItem
+                
+                className="px-3 py-2 text-sm cursor-pointer"
+              >
+                Create Channel
+                <PlusCircle className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+            {isModerator && (
+              <DropdownMenuSeparator />
+            )}
+            {isAdmin && (
+              <DropdownMenuItem
+                className="px-3 py-2 text-sm cursor-pointer text-rose-500"
+              >
+                Delete Server
+                <Trash className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+            {!isAdmin && (
+              <DropdownMenuItem
+                className="px-3 py-2 text-sm cursor-pointer text-rose-500"
+              >
+                Leave Server
+                <LogOut className="w-4 h-4 ml-auto" />
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
         </DropdownMenu>
-     );
-}
+      )
+    }
+
  
