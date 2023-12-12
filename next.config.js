004865447add6648b41010c7b692a38724191bd7
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-12-13 02:58:46 +0600

    undone  next.config change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  webpack: (config)=>{
-    config.externals.push({
-      "utf-8-validate":"commonjs utf-8-validate",
-      bufferutil:"commonjs bufferutil"
-    })
-    return config
-  },
+  
     images: {
       remotePatterns: [
         {
